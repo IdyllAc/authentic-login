1d311fe11b3fd2ebed0a8a28d2fc00d7d138a7f3
Stk1963 <92991243+Stk1963@users.noreply.github.com>
2024-05-13 11:40:41 +0200

    first commit

---
+++ passport-config.js
@@ -0,0 +1,35 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt =require('bcrypt')
+
+function initialize(passport, getUserByEmail, getUserById) {
+    // Function to authenticate users
+    const authenticateUser = async (email, password, done) => {
+        // Get users by email
+        const user = getUserByEmail(email)
+        if (user == null) {
+            return done(null, false, {message: '!لا يوجد إيميل بإسم هذا المستخدم' })
+        }
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false, { message: '!الإيميل او كلمة المرور غير صحيحة' })
+            }
+        } catch (e) {
+            return done(e)
+     }
+
+}
+     passport.use(new LocalStrategy({ usernameField: 'email' },
+     authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))   //{ })
+    passport.deserializeUser((id, done) => {                       //{ })
+       return done(null, getUserById(id))
+})
+}
+module.exports = initialize
+
+
+
+
---
+++ server.js
@@ -0,0 +1,112 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+// Importing libraries that we installed using npm
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt') 
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+// const router = require("express").Router();
+const methodOverride = require('method-override')
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id))
+
+ const users = []
+
+ // Set Views
+app.set('views', 'views')
+//  app.engine('ejs', ejs.renderFile);
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, 
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     methods: "GET, POST, PUT, DELETE",
+//     credentials: true,
+// })
+// );
+
+
+
+app.get('/', checkAuthenticated, (req, res) => {
+    // data={email:'test@test.com', adress:'noida', skills:['node js','php','java']}
+    res.render('index', { name: req.user.name });
+    // console.warn('req.params.name');
+ })
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+     successRedirect: '/',
+     failureRedirect: '/login', 
+     failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+      console.log(users)
+})
+
+ app.delete('/logout?', (req, res) => {
+     req.logOut(req.user, err => {
+         if (err) return next(err)
+         res.redirect('/login')
+         console.log('logged out')
+    })
+
+  })
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+}
+
+
+// listen on port 3000
+app.listen(3000)
+
+
+
